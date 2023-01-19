05a25db159e2f067572755fe211bc82d285e1388
Linus <itslinnet@gmail.com>
2023-01-19 11:37:07 +0100

    Added reset function to clock

--- Clock.gd
+++ Clock.gd
@@ -1,19 +1,35 @@
 extends Node2D
 
-var displayValue = 0
+var value = 0
 onready var timer: Timer = get_node("Timer")
 
 
+# Init
 func _ready() -> void:
 	timer.set_wait_time(1)
 	timer.start()
 	_updateTimer()
 
 
+# Timer timeout happens every timer tick
 func _on_Timer_timeout():
-	displayValue += 1
+	value += 1
 	_updateTimer()
 
 
 func _updateTimer():
-	$Control/RichTextLabel.bbcode_text = "[center] Time: " + str(displayValue)
+	$Control/RichTextLabel.bbcode_text = "[center] Time: " + str(value)
+
+
+func _resetTimer():
+	if Input.is_action_pressed("ui_select"):
+		timer.stop()
+		value = 0
+		_updateTimer()
+		timer.start()
+
+
+# Helper function to reset timer. This should not be used in the actual game.
+func _input(event):
+	if event.is_action_pressed("ui_select"):
+		_resetTimer()
