dd66aff5ce2c473f799ea48de85fb22979c5da95
Emil Bertholdsson <carlemilamandus@gmail.com>
2023-01-20 10:10:55 +0100

    Drift smoke, and WASD

--- Tractor.gd
+++ Tractor.gd
@@ -6,7 +6,6 @@ const ACCELERATOIN = 0.06
 const ROTATION_SPEED = 0.08
 const minWheelAngle = 0.05
 const driftResistance = 20
-const maxSpeed = 700
 var velocity = Vector2()
 var rot = 0
 
@@ -22,15 +21,15 @@ func _physics_process(delta):
 	var res = 4
 	var inSpeed = 0
 	var direction = Vector2(0, 0)	
-	if Input.is_action_pressed("ui_right"): # Rotate the player rightwards
+	if Input.is_action_pressed("steer_right"):
 		rotation += ROTATION_SPEED
 		rot += ROTATION_SPEED
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("steer_left"):
 		rotation -= ROTATION_SPEED
 		rot -= ROTATION_SPEED
-	if Input.is_action_pressed("ui_down"): 
+	if Input.is_action_pressed("break"): 
 		res += 15
-	if Input.is_action_pressed("ui_up"):
+	if Input.is_action_pressed("gas"):
 		inSpeed = 1			
 
 	if inSpeed !=0:
@@ -40,10 +39,16 @@ func _physics_process(delta):
 		# accelerate when there's input
 		var angle = velocity.angle()-(rotation - PI/2) #diffrence in angle between the wheels and the front of the tractor (-pi/2 is from the wheels default rotation)
 		
-		if abs(sin(angle)) > minWheelAngle: #Is the turn big enough to give a drift
-			velocity += Vector2(sin(angle) * driftResistance, 0).rotated(rotation) 
+		if abs(sin(angle)) > minWheelAngle: #Is the turn big enough to give a drift, then drift
+			velocity += Vector2(sin(angle) * driftResistance, 0).rotated(rotation)
+			# Activate drift smoke
+			$DriftSmoke1.emitting = true
+			$DriftSmoke2.emitting = true
+
 		else:
 			velocity *= abs(cos(angle))
+			$DriftSmoke1.emitting = false
+			$DriftSmoke2.emitting = false
 		
 		velocity -= velocity.normalized() * res #apply breaking
 		
@@ -53,11 +58,16 @@ func _physics_process(delta):
 		velocity.y = lerp(velocity.y, 0, FRICTION)
 		
 		velocity -= velocity.normalized() * res #apply breaking
+	
+	if velocity.length() < 200:
+		$DriftSmoke1.emitting = false
+		$DriftSmoke2.emitting = false
+
 	# Apply movement
 	var collision = move_and_collide(velocity*delta)
 	if collision:
 		if velocity.length() > 300:
-			velocity = collision.normal * velocity.length()
+			velocity = collision.normal * velocity.length()/2
 		else:
 			velocity = velocity.slide(collision.normal)
 
